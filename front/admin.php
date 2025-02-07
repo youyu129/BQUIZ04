21369e8a8fff199e7cd36b7d27df7224f0d1c1ce
youyu <129youyu@gmail.com>
2025-02-07 11:39:15 +0800

    新增前台lonin和admin畫面

---
+++ front/admin.php
@@ -0,0 +1,25 @@
+<h2>管理者登入</h2>
+<table class="all">
+    <tr>
+        <td class="tt ct">
+            帳號
+        </td>
+        <td class="pp">
+            <input type="text" name="acc" id="acc">
+        </td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">驗證碼</td>
+        <td class="pp"><input type="text" name="" id=""></td>
+    </tr>
+</table>
+
+<div class="ct">
+    <button>
+        確認
+    </button>
+</div>
---
+++ front/login.php
@@ -0,0 +1,27 @@
+<h2>第一次購物</h2>
+<img src="../icon/0413.jpg" alt="">
+<h2>會員登入</h2>
+<table class="all">
+    <tr>
+        <td class="tt ct">
+            帳號
+        </td>
+        <td class="pp">
+            <input type="text" name="acc" id="acc">
+        </td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">驗證碼</td>
+        <td class="pp"><input type="text" name="" id=""></td>
+    </tr>
+</table>
+
+<div class="ct">
+    <button>
+        確認
+    </button>
+</div>
