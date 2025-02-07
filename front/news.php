085f1f1d04417348665f42a16eb922ddfc430144
youyu <129youyu@gmail.com>
2025-02-07 11:29:45 +0800

    新增前台news的兩個詳細內容表格

--- front/news.php
+++ front/news.php
@@ -28,6 +28,27 @@
     </tr>
 </table>
 
+<table class="all news" id="news1">
+    <tr>
+        <td class="tt">標題</td>
+        <td class="pp">年終特賣會開跑了</td>
+    </tr>
+    <tr>
+        <td class="tt">內容</td>
+        <td class="pp">即日期至年底，凡會員購物滿仟送佰，買越多送越多~</td>
+    </tr>
+</table>
+
+<table class="all news" id="news2">
+    <tr>
+        <td class="tt">標題</td>
+        <td class="pp">情人節特惠活動</td>
+    </tr>
+    <tr>
+        <td class="tt">內容</td>
+        <td class="pp">為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~</td>
+    </tr>
+</table>
 <!-- 最新消息1
 標題
 年終特賣會開跑了
@@ -45,3 +66,9 @@
 
 內容
 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~ -->
+
+<script>
+$(".title").on("click", function() {
+
+})
+</script>
