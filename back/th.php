a4e94dbca1a3d8ad95ec46eb3153a78fd76b5b72
youyu <129youyu@gmail.com>
2025-02-13 13:26:46 +0800

    完成商品分類與管理畫面與假資料

---
+++ back/th.php
@@ -0,0 +1,90 @@
+<h2 class="ct">商品分類</h2>
+
+<div class="ct">
+    新增大分類
+    <input type="text" name="big" id="big">
+    <button>
+        新增
+    </button>
+</div>
+<div class="ct">
+    新增中分類
+    <select name="selbig" id="selbig">
+        <option>1</option>
+        <option>2</option>
+    </select>
+    <input type="text" name="mid" id="mid">
+    <button>
+        新增
+    </button>
+</div>
+
+<table class="all">
+    <tr>
+        <td class="tt">流行皮件</td>
+        <td class="tt ct">
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="pp ct">女用皮件</td>
+        <td class="pp ct">
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="pp ct">男用皮件</td>
+        <td class="pp ct"> <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="tt">流行鞋區</td>
+        <td class="tt ct"> <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="pp ct">少女鞋區</td>
+        <td class="pp ct"> <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="pp ct">紳士流行鞋區</td>
+        <td class="pp ct"> <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+</table>
+
+
+<h2 class="ct">商品管理</h2>
+
+<table class="all">
+    <tr class="tt">
+        <td class="ct">編號</td>
+        <td class="ct">商品名稱</td>
+        <td class="ct">庫存量</td>
+        <td class="ct">狀態</td>
+        <td class="ct">操作</td>
+    </tr>
+    <tr class="pp">
+        <td class="ct">編號</td>
+        <td class="">商品名稱</td>
+        <td class="ct">庫存量</td>
+        <td class="ct">狀態</td>
+        <td class="ct">
+            <div>
+                <button>修改</button>
+                <button>刪除</button>
+            </div>
+            <div>
+                <button>上架</button>
+                <button>下架</button>
+            </div>
+        </td>
+    </tr>
+</table>
