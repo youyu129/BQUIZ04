6eeaa7473279badaaf5f14ba8689712504512e37
youyu <129youyu@gmail.com>
2025-02-13 10:25:02 +0800

    在後台判斷管理者帳號的權限 有權限的項目才顯示出來

--- back.php
+++ back.php
@@ -1,6 +1,9 @@
 <?php
 include_once "api/db.php";
+$user=$Admin->find(['acc'=>$_SESSION['Admin']]);
+$pr=unserialize($user['pr']);
 ?>
+
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0057)?do=admin -->
@@ -26,11 +29,14 @@ include_once "api/db.php";
         <div id="left" class="ct">
             <div style="min-height:400px;">
                 <a href="?do=admin">管理權限設置</a>
-                <a href="?do=th">商品分類與管理</a>
-                <a href="?do=order">訂單管理</a>
-                <a href="?do=mem">會員管理</a>
-                <a href="?do=bot">頁尾版權管理</a>
-                <a href="?do=news">最新消息管理</a>
+
+                <?php
+                echo (in_array(1,$pr))?"<a href='?do=th'>商品分類與管理</a>":'';
+                echo (in_array(2,$pr))?"<a href='?do=order'>訂單管理</a>":'';
+                echo (in_array(3,$pr))?"<a href='?do=mem'>會員管理</a>":'';
+                echo (in_array(4,$pr))?"<a href='?do=bot'>頁尾版權管理</a>":'';
+                echo (in_array(5,$pr))?"<a href='?do=news'>最新消息管理</a>":'';
+                ?>
                 <a href="./api/logout.php?table=Admin" style="color:#f00;">登出</a>
             </div>
         </div>
