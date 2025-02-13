f843ae03d64f3d0a85715b51e4c04c20da6e63c3
youyu <129youyu@gmail.com>
2025-02-13 15:49:18 +0800

    完成刪除功能

--- js/js.js
+++ js/js.js
@@ -1,5 +1,12 @@
 // JavaScript Document
-function lof(x)
-{
-	location.href=x
+function lof(x) {
+	location.href = x
+}
+
+function del(table, id) {
+	$.post("./api/del.php", { table, id }, function (res) {
+		// console.log('res',res);
+
+		location.reload()
+	})
 }
