11de943c012a092d37409809448e2b51c0e264b3
youyu <129youyu@gmail.com>
2025-02-14 14:11:19 +0800

    完成新增商品的功能

---
+++ api/save_item.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once "db.php";
+
+if(isset($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
+    $_POST['img']=$_FILES['img']['name'];
+}
+
+if(!isset($_POST['id'])){
+    $_POST['sh']=1;
+}
+
+$Item->save($_POST);
