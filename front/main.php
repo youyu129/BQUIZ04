d28428b406cd83a86ed5f41dfb3c90d72388836b
youyu <129youyu@gmail.com>
2025-02-17 10:49:51 +0800

    在前台顯示出全部商品的圖片及資料

--- front/main.php
+++ front/main.php
@@ -1,5 +1,5 @@
 <?php
-$nav=[];
+$nav='';
 $typeId=$_GET['type']??0;
 if($typeId==0){
     $nav="全部商品";
@@ -9,9 +9,39 @@ if($typeId==0){
         $nav=$type['name'];
     }else{
         $big=$Type->find($type['big_id']);
-        $nav=$big['name'] . ">" . $type['name'];
+        $nav=$big['name'] ." > ". $type['name'];
     }
 }
 ?>
 
 <h2><?=$nav;?></h2>
+
+<?php
+$rows=$Item->all([]);
+if($typeId==0){
+    $rows=$Item->all();
+}else if($type['big_id']==0){
+    $rows=$Item->all(['big'=>$typeId]);
+}else{
+    $rows=$Item->all(['mid'=>$typeId]);
+}
+?>
+<?php
+foreach($rows as $row):
+?>
+<div class="item">
+    <div class="pp">
+        <a href="?do=detail&id=<?=$row['id'];?>">
+            <img src="./img/<?=$row['img'];?>" alt="" style="width:200px;height:160px;">
+        </a>
+    </div>
+    <div>
+        <div class="tt ct"><?=$row['name'];?></div>
+        <div class="pp">價錢：<?=$row['price'];?></div>
+        <div class="pp">規格：<?=$row['spec'];?></div>
+        <div class="pp">簡介：<?=mb_substr($row['intro'],0,20);?>...</div>
+    </div>
+</div>
+<?php
+endforeach;
+?>
