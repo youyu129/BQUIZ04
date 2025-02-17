663c3cdfab645db678a7d578e3f2f251305d53f0
youyu <129youyu@gmail.com>
2025-02-17 10:50:56 +0800

    修改api的save_item isset改為!empty

--- api/save_item.php
+++ api/save_item.php
@@ -2,7 +2,7 @@
 
 include_once "db.php";
 
-if(isset($_FILES['img']['tmp_name'])){
+if(!empty($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
     $_POST['img']=$_FILES['img']['name'];
 }
