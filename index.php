4f009022f11154026987c6ff556f42a53ebb0053
youyu <129youyu@gmail.com>
2025-02-21 08:59:39 +0800

    使用ajax加上即時顯示購車商品數量提示 畫面載入時就先確認是否有購物車的SESSUION

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-include_once "api/db.php";
-?>
+<?php include_once "api/db.php"; ?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
@@ -16,82 +14,71 @@ include_once "api/db.php";
 </head>
 
 <body>
+
     <div id="main">
         <div id="top">
             <a href="?">
-                <img src="./icon/0416.jpg" style="width:500px;">
+                <img src="./icon/0416.jpg" style="width:500px">
             </a>
-            <div style="padding:10px;display: inline-block;vertical-align: top;">
+            <div style="padding:10px;display:inline-block;vertical-align:top;">
                 <a href="?">回首頁</a> |
                 <a href="?do=news">最新消息</a> |
                 <a href="?do=look">購物流程</a> |
-                <a href="?do=buycart">購物車</a> |
+                <a href="?do=buycart">購物車<span id='items'></span></a> |
                 <?php
-                if(empty($_SESSION['Mem'])){
-
+                    if (empty($_SESSION['Mem'])) {
                     ?>
                 <a href="?do=login">會員登入</a> |
                 <?php
-                }else{
-                ?>
+                    } else {
+                    ?>
                 <a href="./api/logout.php?table=Mem">登出</a> |
                 <?php
-                }
+                    }
                 ?>
-
+<?php
+    if (empty($_SESSION['Admin'])) {
+    ?>
+                <a href="?do=admin">管理登入</a> |
                 <?php
-                if(empty($_SESSION['Admin'])){
-
+                    } else {
                     ?>
-                <a href="?do=admin">管理登入</a>
+                <a href="back.php">返回管理</a> |
                 <?php
-                }else{
+                    }
                 ?>
-                <a href="back.php">返回後台</a> |
-                <?php
-                }
-                ?>
+
             </div>
 
+
         </div>
+
         <div id="left" class="ct">
             <div style="min-height:400px;">
+                <a href="?type=0">全部商品(<?php echo $Item->count(['sh' => 1]);?>)</a>
                 <?php
-                $bigs=$Type->all(['big_id'=>0]);
-                echo "<div class='ww'>";
-                echo "<a href='?type=0'>";
-                echo "全部商品";
-                echo "(".$Item->count(['sh'=>1]).")";
-                echo "</a>";
-                echo "</div>";
-
-                foreach($bigs as $big){
-                    echo "<div class='ww'>";
-                    echo "<a href='?type={$big['id']}'>";
-                    echo $big['name'];
-                    echo "({$Item->count(['big'=>$big['id'],'sh'=>1])})";
-                    echo "</a>";
-
-                    if($Type->count(['big_id'=>$big['id']])>0){
-                        $mids=$Type->all(['big_id'=>$big['id']]);
-                        echo "<div class='s'>";
-                        foreach($mids as $mid){
-                            echo "<a href='?type={$mid['id']}' style='background:lightblue'>";
-                            echo $mid['name'];
-                            echo "({$Item->count(['mid'=>$mid['id'],'sh'=>1])})";
-                            echo "</a>";
+                    $bigs = $Type->all(['big_id' => 0]);
+                    foreach ($bigs as $big) {
+                        echo "<div class='ww'>";
+                        echo "<a href='?type={$big['id']}'>";
+                        echo $big['name'];
+                        echo "({$Item->count(['big' => $big['id'], 'sh' => 1])})";
+                        echo "</a>";
+                        if ($Type->count(['big_id' => $big['id']]) > 0) {
+                            $mids = $Type->all(['big_id' => $big['id']]);
+                            echo "<div class='s'>";
+                            foreach ($mids as $mid) {
+                                echo "<a href='?type={$mid['id']}' style='background-color: #7ee185;'>";
+                                echo $mid['name'];
+                                echo "({$Item->count(['mid' => $mid['id'], 'sh' => 1])})";
+                                echo "</a>";
+                            }
+                            echo "</div>";
                         }
                         echo "</div>";
-
                     }
-
-
-                    echo "</div>";
-                }
                 ?>
-                <div class="s">
-                    123
-                </div>
+
             </div>
             <span>
                 <div>進站總人數</div>
@@ -101,22 +88,29 @@ include_once "api/db.php";
         </div>
         <div id="right">
             <?php
-                $do=$_GET['do']??'main';
-                $file="front/".$do.".php";
-                if(file_exists($file)){
-                        include $file;
-                }else{
-                        include "front/main.php";
+                $do   = $_GET['do'] ?? 'main';
+                $file = "front/" . $do . ".php";
+                if (file_exists($file)) {
+                    include $file;
+                } else {
+                    include "front/main.php";
                 }
 
-                ?>
+            ?>
         </div>
-
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
-            <?=$Bot->find(1)['bottom'];?>
-        </div>
+            <?php echo $Bot->find(1)['bottom'];?></div>
     </div>
 
 </body>
 
 </html>
+
+<script>
+$.get("api/buycart.php", function(count) {
+    console.log(count);
+    if (parseInt(count) > 0) {
+        $("#items").text(`(${count})`);
+    }
+})
+</script>
