1a1f644d7f23f315fc9a38df7b52de22929a4bc1
youyu <129youyu@gmail.com>
2025-02-24 13:52:57 +0800

    將登入畫面的計數驗證碼 改為圖形驗證碼 並加入換一組驗證碼的按鈕

--- front/login.php
+++ front/login.php
@@ -2,7 +2,7 @@
 <a href="?do=reg">
     <img src="../icon/0413.jpg" alt="">
 </a>
-<!-- <?=serialize([1,2,3,4,5]);?> -->
+<!--                     <?php echo serialize([1, 2, 3, 4, 5]); ?> -->
 <h2>會員登入</h2>
 <table class="all">
     <tr>
@@ -20,12 +20,13 @@
     <tr>
         <td class="tt ct">驗證碼</td>
         <td class="pp">
-            <?php
-            $a=rand(10,99);
-            $b=rand(10,99);
-            $_SESSION['ans']=$a+$b;
-            echo $a."+".$b."=";
-            ?>
+            <div style="display:flex">
+                <div
+                    style="vertical-align:middle;display:flex;width:180px;height:55px;justify-content:center;align-items:center;background-color:white">
+                    <img src="" alt="" id="chapcha">
+                </div>
+                <button onclick="getChapcha()">換一組驗證碼</button>
+            </div>
             <input type="text" name="ans" id="ans">
         </td>
     </tr>
@@ -38,6 +39,14 @@
 </div>
 
 <script>
+getChapcha();
+
+function getChapcha() {
+    $.get("./api/code.php", function(res) {
+        $("#chapcha").attr("src", res)
+    })
+}
+
 function login() {
     let ans = $("#ans").val()
     $.get("./api/chk_ans.php", {
@@ -56,11 +65,12 @@ function login() {
                     location.href = "index.php"
                 } else {
                     alert("帳號或密碼錯誤")
+                    getChapcha();
                 }
             })
         } else {
             alert("驗證碼錯誤，請重新輸入")
-
+            getChapcha();
         }
     })
 }
